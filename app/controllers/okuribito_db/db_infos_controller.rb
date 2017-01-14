fb87eb28660c89d50a714039f791910c40a45305
ym <kiyoshi.kodama@gmail.com>
2017-01-14 20:02:15 +0900

    List view(with pagination) #1

--- app/controllers/okuribito_db/db_infos_controller.rb
+++ app/controllers/okuribito_db/db_infos_controller.rb
@@ -3,7 +3,7 @@ require_dependency "okuribito_db/application_controller"
 module OkuribitoDb
   class DbInfosController < ApplicationController
     def index
-      @db_infos = DbInfo.all.order("id DESC")
+      @db_infos = DbInfo.page(params[Kaminari.config.param_name]).order("id DESC")
       @db_info_ids = @db_infos.pluck(:id)
       @table_infos = TableInfo.bundle(@db_info_ids).group_by(&:table_name)
     end
--- app/models/okuribito_db/db_info.rb
+++ app/models/okuribito_db/db_info.rb
@@ -1,5 +1,7 @@
 module OkuribitoDb
   class DbInfo < ActiveRecord::Base
     has_many :table_infos
+
+    paginates_per 5
   end
 end
